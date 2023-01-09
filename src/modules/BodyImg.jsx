8691b535a4e5e2b2397ed9431debc10b7440d9a1
Oscar Recén Larsson <112632742+oscarrecenlarsson@users.noreply.github.com>
2023-01-09 16:10:07 +0100

    mappstruktur
    nu funkar grunden

---
+++ src/modules/BodyImg.jsx
@@ -0,0 +1,9 @@
+import waterImg from "../img/water.jpg"
+
+export default function BodyImg() {
+    return (
+        <div>
+            <img src={waterImg} className="water" />
+        </div>
+    )
+} 
