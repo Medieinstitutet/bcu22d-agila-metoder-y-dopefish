b59dbe43e2f820efef3a892777c43984fdaa5998
ZarinaMye <113510101+ZarinaMye@users.noreply.github.com>
2023-01-09 15:06:05 +0100

    initial

--- README.md
+++
@@ -1,9 +0,0 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=9675742&assignment_repo_type=AssignmentRepo)
-# BCU22D Agila Metoder
-Mall-repo för Agila metoder, Blockchainutvecklare 2022.
-
-Använd mappen `scrum-ceremonies` för att föra logg & redovisa enligt givna riktlinjer.
-
-Resten av koden kan ni skriva direkt i repot på valfritt sätt.
-
-..
---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import BodyImg from "./BodyImg"
+
+function Header () {
+  return (
+      <header className="header">
+          <a href="https://discord.com/channels/1060149946612981871/1060149947078557718" target="_blank">Join Dopefish on Discord</a>
+      </header>
+  )
+}
+
+function Footer () {
+  return (
+      <footer className="footer">
+          <button className="homeBtn">HOME</button> 
+          <button className="teamBtn">TEAM</button>
+          <button className="roadMapBtn">ROADMAP</button> 
+          <button className="nftBtn">NFTs</button> 
+          <button className="merchBtn">MERCH</button> 
+     </footer>
+  )
+}
+
+export default function HomePage () {
+  return (
+  <div>
+      <BodyImg />
+      <Header/>
+      <button className="buyBtn">Buy Dopefish Token</button>
+      <img src="./token.gif" className="token"/>
+      <p className="textBuy">"Placeholder, explain how to buy Dopefish token"</p>
+      <Footer />
+  </div>
+  )
+}
+
+/* function TeamPage () {
+  return (
+
+  )
+}
+function RoadMapPage () {
+  return (
+      
+  )
+}
+function NftPage () {
+  return (
+      
+  )
+}
+function MerchPage () {
+  return (
+      
+  )
+}
+*/
+
+
+
+/* ReactDOM.render(
+  <div>
+      <h1 className="header">Hello</h1>
+      <p>Det funkar också ganska bra...</p>
+  </div>, 
+  document.querySelector("#root")
+) */
+
+/* const h1 = document.createElement("h1")
+h1.textContent = "Hello"
+h1.className = "header"
+document.getElementById("root").append(h1) */
+
