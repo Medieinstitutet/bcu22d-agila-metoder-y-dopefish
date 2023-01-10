a1807a3769924e15165903306221ccf740e786fc
ZarinaMye <113510101+ZarinaMye@users.noreply.github.com>
2023-01-10 13:31:15 +0100

    roadmap <p> + css roadmapP o merchP

--- src/modules/MerchPage.jsx
+++ src/modules/MerchPage.jsx
@@ -1,11 +1,11 @@
-import merchImg from "../img/merch.png"
+import MerchImg from "../img/Merch.png"
 
 export default function MerchPage () {
     return (
         <div>
             <h2 className="Merch">Merch collection</h2>
             <h1 className="Coming2">COMING SOON!</h1>
-            <img src={merchImg} className="MerchPic" />
+            <img src={MerchImg} className="MerchPic" />
         </div>
     )
   }
--- src/modules/RoadmapPage.jsx
+++ src/modules/RoadmapPage.jsx
@@ -2,10 +2,16 @@ export default function RoadmapPage () {
     return (
         <div>
             <h2 className="Roadmap">Our plan</h2>
-            <p className="Text">January 2023</p>
-            <p>Initial Token Offering{/* </br> */}
-            Publish webbpage</p>
-            <p className="Text2">March 2023{/* <br> */}Launch merch collection{/* </br> */}Launch NFTs</p>
+            <div className="Text">
+                <p>January 2023</p>
+                <p>Initial Token Offering</p>
+                <p>Publish webbpage</p>
+            </div>
+            <div className="Text2">
+                <p>March 2023</p>
+                <p>Launch merch collection</p>
+                <p>Launch NFTs</p>
+            </div>
         </div>
     )
   }
