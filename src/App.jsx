39046ab27e196463668017c58d4266b22ad150ae
ZarinaMye <113510101+ZarinaMye@users.noreply.github.com>
2023-01-10 11:26:43 +0100

    HomePage + <br></br> utkom. i road.P behöver lösas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import BodyImg from "./modules/BodyImg"
 import Header from "./modules/Header"
-import tokenImg from "./img/token.gif"
 import Footer from "./modules/Footer"
-/* import MerchPage from "./modules/MerchPage"
+import HomePage from "./modules/HomePage"
+import MerchPage from "./modules/MerchPage"
 import NftPage from "./modules/NftPage"
 import RoadmapPage from "./modules/RoadmapPage"
-import TeamPage from "./modules/TeamPage" */
+import TeamPage from "./modules/TeamPage"
 
-export default function HomePage() {
+export default function Page() {
   return (
     <div>
       <BodyImg />
       <Header />
-      <button className="buyBtn">Buy Dopefish Token</button>
-      <img src={tokenImg} className="token" />
-      <p className="textBuy">"Placeholder, explain how to buy Dopefish token"</p>
+      <HomePage />
       <Footer />
     </div>
   )
