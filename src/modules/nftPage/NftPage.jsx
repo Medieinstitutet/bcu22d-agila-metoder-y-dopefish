8940af30d04e750dd9ac2bdc03a7d786491a12b0
Oscar Recén Larsson <112632742+oscarrecenlarsson@users.noreply.github.com>
2023-01-15 17:46:25 +0100

    cleaned up the code

--- src/modules/nftPage/NftPage.jsx
+++ src/modules/nftPage/NftPage.jsx
@@ -4,102 +4,24 @@ import React, { useEffect, useState } from "react";
 
 export default function NftPage() {
 
-    //Hook
-    // function getWidth() {
-    //     // Initialize state with undefined width/height so server and client renders match
-    //     // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    //     const [width, setWidth] = useState(undefined);
-    //     useEffect(() => {
-    //         // Handler to call on window resize
-    //         function handleResize() {
-    //             // Set window width/height to state
-    //             setWidth(window.innerWidth);
-    //         }
-    //         // Add event listener
-    //         window.addEventListener("resize", handleResize);
-    //         // Call handler right away so state gets updated with initial window size
-    //         handleResize();
-    //         // Remove event listener on cleanup
-    //         return () => window.removeEventListener("resize", handleResize);
-    //     }, []); // Empty array ensures that effect is only run on mount
-    //     return width;
-    // }
-
-    // let width = getWidth()
-    // console.log(width);
-
-    // const [width, setWidth] = useState(window.innerWidth);
-    // const handleResize = () => {
-    //     // Set window width/height to state
-    //     setWidth(window.innerWidth);
-    //     // console.log(width);
-    //     // console.log(window.innerWidth);
-    //     // setNumberOfNfts(getNumberOfNfts(width))
-    // }
-
-    // useEffect(() => {
-    //     window.addEventListener("resize", handleResize)
-    // }, [])
-
-
-
-    // le width = useWindowSize()
-    // console.log("width", width);
-
-
-    // const [numberOfNfts, setNumberOfNfts] = useState(3);
-    // console.log(numberOfNfts);
-    // const [width, setWidth] = useState(size);
-    // console.log("LSKJDFLKJSD", width);
-
     function getNumberOfNfts(width) {
         if (width <= 813) {
-            // setNumberOfNfts(1)
-            console.log("number 1");
             return 1
         }
         else if (width <= 1250) {
-            // setNumberOfNfts(2)
-            console.log("number 2");
             return 2
         }
         else {
-            console.log("number 3");
-            // setNumberOfNfts(3)
             return 3
         }
-
-
-        //     // return setNumberOfNfts(2)
-        //     // //return 3
     }
-
-
-
-    //const numberOfNfts = getNumberOfNfts(size.width);
-
-    // getNumberOfNfts(size.width)
-
-    // let start = 0;
-    // let end = numberOfNfts;
-
-    // let width = 200;
-    // setWidth({ value: 500 })
-    //getNumberOfNfts(width)
-
 
     const [numberOfNfts, setNumberOfNfts] = useState(() => {
         let num = getNumberOfNfts(window.innerWidth);
         return num
     })
 
-
-
     const handleResize = () => {
-        // Set window width/height to state
-        // setWidth(window.innerWidth);
-        // console.log(width);
-        // console.log(window.innerWidth);
         setNumberOfNfts(() => {
             let num = getNumberOfNfts(window.innerWidth);
             setStart(0)
@@ -112,24 +34,12 @@ export default function NftPage() {
         window.addEventListener("resize", handleResize)
     }, [])
 
-
-
-    // getNumberOfNfts(width)
-
-
-    //let numberOfNfts = 1;
-
-
     const [data, setData] = useState(nftData)
-    // const [pagination, setPagination] = useState({ start: start, end: end })
-
     const [start, setStart] = useState(0)
     const [end, setEnd] = useState(numberOfNfts)
 
-    //SETEND START+NUMBEROFNFTS
-
     const nfts = data.slice(start, end).map(nft => {
-        console.log(start, end);
+        // console.log(start, end);
         return (
             <Nft
                 name={nft.name}
@@ -145,19 +55,9 @@ export default function NftPage() {
         if (end <= (data.length - numberOfNfts)) {
             setStart(prevValue => prevValue + numberOfNfts)
             setEnd(prevValue => prevValue + numberOfNfts)
-            // start += numberOfNfts
-            // end += numberOfNfts
-            // setPagination({ start: start, end: end })
-            // console.log(start);
-            // console.log(end);
-            // console.log("page.end", pagination.end);
         } else if (data.length % numberOfNfts != 0 && end < data.length) {
-            // setStart(end)
             setStart(end)
             setEnd(data.length)
-            // console.log("EN UDDA KVAR!");
-            // setStart(data.length - 1)
-            // setEnd(data.length)
         }
     }
 
@@ -173,9 +73,6 @@ export default function NftPage() {
                 setEnd(prevValue => prevValue - numberOfNfts)
             }
     }
-
-
-
 
     return (
         <div className="nftPage">
